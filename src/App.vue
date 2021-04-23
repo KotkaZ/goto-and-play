2ce652cd25e5e7a27aa783a0a132cfd68d3cb4cc
Karl Rapur <rapurkarl@gmail.com>
2021-04-23 16:58:06 +0300

    Changed About component.

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@ import "primevue/resources/themes/saga-blue/theme.css"; //theme
 import "primevue/resources/primevue.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
 import "primeflex/primeflex.css";
+export default {
+  name: "app",
+};
 </script>
 
 <style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <Card class="p-shadow-14">
+    <template #content>
+      <h1>This is gotoAndPlay internship task.</h1>
+      <h2>
+        Karl Rapur <br />
+        karl.rapur@ut.ee
+      </h2>
+    </template>
+  </Card>
 </template>
+
+<script>
+import Card from "primevue/card";
+export default {
+  components: {
+    Card,
+  },
+};
+</script>
+
+<style></style>
